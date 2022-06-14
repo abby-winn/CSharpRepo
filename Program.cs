80b1f0ee00875078223f05f6975b53cb9289213a
Tommy Terry <tommyterry2005@gmail.com>
2022-06-14 10:24:37 -0500

    Updated the program.cs file

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Tommy Terry");
+Console.WriteLine("I don't remember the other questions :/");
