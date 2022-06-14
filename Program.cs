316fdde0ca224adcec42d3cd7e9a2cedb798a99a
Alan Sudhan <23sudhanalan@gmail.com>
2022-06-14 10:25:21 -0500

    Printed my name, school, and gift

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine(“Name: Alan J. Sudhan”);
+Console.WriteLine(“School: Brookfield East Highschool”);
+Console.WriteLine(“Worst Gift: A Sweater”);
