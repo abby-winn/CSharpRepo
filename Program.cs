1f6c858c968094076a897fe5e2de5c732b37efd9
Alan Sudhan <23sudhanalan@gmail.com>
2022-06-14 10:47:11 -0500

    Added My Graduation Gift

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Kartik Ramachandrula, Rising Sophomoree, Brookfield Central.");
-Console.WriteLine("Hello, World!");
+Console.WriteLine("I would like a full ride scholarship from Elon Musk");
 Console.WriteLine("Tommy Terry");
 Console.WriteLine("I don't remember the other questions :/");
-
 Console.WriteLine("Aman Amjad BCHS'24");
